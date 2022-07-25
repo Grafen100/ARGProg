e176efed8266c23251c7dbe5dc3176de971c0d21
Grafen100 <99442821+Grafen100@users.noreply.github.com>
2022-07-25 16:21:42 -0300

    creation skills component

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AboutComponent } from './components/about/about.component';
 import { EducationComponent } from './components/education/education.component';
 import { ExperiencesComponent } from './components/experiences/experiences.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ProjectsComponent } from './components/projects/projects.component';
     AboutComponent,
     EducationComponent,
     ExperiencesComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
